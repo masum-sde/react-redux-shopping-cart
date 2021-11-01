de5b88241ac3b7b25206d2d62d26a54baf3f4746
Masum-Web-Developer <masum.sde@gmail.com>
2021-11-01 15:55:23 +0600

    animation-modal component done

--- src/components/CartItems/CartItems.jsx
+++ src/components/CartItems/CartItems.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import CartItem from "../CartItem/CartItem";
 import "./CartItems.css";
 import formatCurrency from "./../../util";
+import Fade from "react-reveal/Fade";
 const CartItems = ({ cartItems, handleRemoveCartItem, handleOrderedData }) => {
   const totalPrice = cartItems?.reduce((sum, item) => (sum += item.count * item.price), 0);
   const [isCheckout, setIsCheckout] = useState(false);
@@ -29,7 +30,9 @@ const CartItems = ({ cartItems, handleRemoveCartItem, handleOrderedData }) => {
       </div>
       <div className="cart-list">
         {cartItems.map((item) => (
-          <CartItem item={item} handleRemoveCartItem={handleRemoveCartItem} />
+          <Fade left collapse>
+            <CartItem item={item} handleRemoveCartItem={handleRemoveCartItem} />
+          </Fade>
         ))}
       </div>
       {cartItems?.length > 0 && (
@@ -42,25 +45,27 @@ const CartItems = ({ cartItems, handleRemoveCartItem, handleOrderedData }) => {
       )}
       {isCheckout && (
         <div className="cart">
-          <form onSubmit={handleCreateOrder}>
-            <div className="form-input-holder">
-              <label htmlFor="">Email</label>
-              <input type="email" name="email" id="" required onChange={handleInput} />
-            </div>
-            <div className="form-input-holder">
-              <label htmlFor="">Name</label>
-              <input type="text" name="name" id="" required onChange={handleInput} />
-            </div>
-            <div className="form-input-holder">
-              <label htmlFor="">Address</label>
-              <input type="text" name="address" id="" required onChange={handleInput} />
-            </div>
-            <div className="form-input-holder">
-              <button className="button primary" type="submit" onClick={handleCreateOrder}>
-                Checkout
-              </button>
-            </div>
-          </form>
+          <Fade right collapse>
+            <form onSubmit={handleCreateOrder}>
+              <div className="form-input-holder">
+                <label htmlFor="">Email</label>
+                <input type="email" name="email" id="" required onChange={handleInput} />
+              </div>
+              <div className="form-input-holder">
+                <label htmlFor="">Name</label>
+                <input type="text" name="name" id="" required onChange={handleInput} />
+              </div>
+              <div className="form-input-holder">
+                <label htmlFor="">Address</label>
+                <input type="text" name="address" id="" required onChange={handleInput} />
+              </div>
+              <div className="form-input-holder">
+                <button className="button primary" type="submit" onClick={handleCreateOrder}>
+                  Checkout
+                </button>
+              </div>
+            </form>
+          </Fade>
         </div>
       )}
     </div>
